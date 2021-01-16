d55189a1334bf157a730b2f4b6d00a66a4a3c501
Dean Coulstock <dean.coulstock@thoughtworks.com>
2021-01-16 17:38:03 +1000

    [DC] Add tests to solution, setup boilerplate test

---
+++ OnlineTitleSearch.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace OnlineTitleSearch.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            // tests go here
+        }
+    }
+}
